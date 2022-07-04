ff89cece2bc558d6b5564b7dcf446e451ac0a458
John <45017704+codemobii@users.noreply.github.com>
2022-07-04 17:14:35 +0100

    Update index.js

--- index.js
+++ index.js
@@ -3,6 +3,9 @@ const app = express();
 const http = require("http").Server(app);
 const path = require("path");
 const cors = require("cors");
+
+const port = process.env.PORT || 5000;
+
 const io = require("socket.io")(http, {
   cors: {
     origin: ["http://localhost:3000", "https://nlsalumni.org"],
